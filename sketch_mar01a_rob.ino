0c5254fcfe7843aa69c6e2fb4f5a6ef1e6fba2ea
robertLopezcoder <47820563+robertLopezcoder@users.noreply.github.com>
2019-03-01 10:26:10 -0800

    Add files via upload

---
+++ sketch_mar01a_rob.ino
@@ -0,0 +1,80 @@
+#include <FastLED.h>
+
+#define NUM_LEDS 135
+
+#define DATA_PIN 11
+
+
+
+CRGB leds[NUM_LEDS];
+
+
+
+void setup() {
+
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+
+}
+
+
+
+void loop() {
+
+
+
+for (int n = 134; n<NUM_LEDS;n+= 134){
+
+  for(int i=0; i < n; i++){
+
+    leds[i] = CRGB::Blue;
+
+    FastLED.show();
+
+    delay(10);
+
+    leds[i] = CRGB::Red;
+
+  }
+
+  for(int i=0;i<n;i++){
+
+    leds[n-i] = CRGB::Blue;
+
+    FastLED.show();
+
+    delay(10);
+
+    leds[n-i] = CRGB::Black;
+
+  }
+
+  for(int i=0;i<n;i++){
+
+    leds[n-i] = CRGB::Blue;
+
+    FastLED.show();
+
+    delay(10);
+
+    leds[n-1] = CRGB::Black;
+
+  }
+
+    for(int dot = 0; dot < NUM_LEDS; dot++) {
+
+    leds[dot] = CRGB::Blue;
+
+    FastLED.show();
+
+
+
+    leds[dot] = CRGB::Black;
+
+    delay(50) ;
+
+    }
+
+  }
+
+}
+
